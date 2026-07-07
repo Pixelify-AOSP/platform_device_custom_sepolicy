LineageOS/android_device_lineage_sepolicy

Import legacy libion policy
This imports the policy relevant to the following commits:

    commit 0123d49a0134a81ee4d2160adf1700ed593144ad
    Author: Devin Moore <devinmoore@google.com>
    Date:   Tue Feb 10 17:19:02 2026 +0000
    Revert "Remove ION sepolicy"

    There are still too many devices using the ion_device type, so this
    needs to be temporarily reverted to unblock other work while devices are
    cleaned up.

    Partial revert of I077d25e4d84a2a56e9210da71a9b2dbfd262124b

    As of January 1, 2026 ION is no longer supported on any Android version,
    device, or configuration.

    Test: m
    Bug: 482058879
    Flag: EXEMPT BUGFIX
    Change-Id: I077d25e4d84a2a56e9210da71a9b2dbfd262124b

    commit 9b2d37dc22ca33bddace586c93824c1a79590ba8
    Author: T.J. Mercier <tjmercier@google.com>
    Date:   Fri Jan 30 11:55:15 2026 -0800
    Remove ION sepolicy

    As of January 1, 2026 ION is no longer supported on any Android version,
    device, or configuration.

    Bug: 447687777
    Change-Id: I50b34be1a5b8a73604b4db87b4d59234a18b8f36

Change-Id: I4aabdb272652ce1a909e43123f8e92fbf22b36e8

libion/sepolicy.mk
@@ -1,0 +1,7 @@
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/libion/vendor
+endif
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/libion/system_ext/private

Pixelify-AOSP/platform_device_custom_sepolicy

libion/sepolicy.mk
@@ -1,0 +1,7 @@
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/custom/sepolicy/libion/vendor
+endif
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+   device/custom/sepolicy/libion/system_ext/private